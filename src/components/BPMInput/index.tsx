3afbd20a24fea990445c2b2a934e18e30023fa46
aereal <aereal@aereal.org>
2018-05-20 22:35:42 +0900

    BPMInput has base beat input

--- src/components/BPMInput/index.tsx
+++ src/components/BPMInput/index.tsx
@@ -2,6 +2,8 @@ import TextField from '@material-ui/core/TextField';
 import * as React from 'react';
 
 import { BPM } from '../../domain/model/BPM';
+import { Duration } from '../../domain/model/Duration';
+import { BeatInput } from '../BeatInput';
 
 // tslint:disable-next-line:interface-name
 interface Props {
@@ -14,13 +16,22 @@ export const BPMInput: React.SFC<Props> = ({ defaultBpm, onChange }) => {
     const bpm = event.target.valueAsNumber;
     onChange(new BPM(defaultBpm.unitBeat, bpm));
   };
+  const onChangeUnitBeat = (unitBeat: Duration) => {
+    onChange(new BPM(unitBeat, defaultBpm.perMinute));
+  };
 
   return (
-    <TextField
-      inputProps={{min: 0}}
-      onChange={handler}
-      defaultValue={defaultBpm.perMinute}
-      label="BPM"
-      type="number" />
+    <div>
+      <TextField
+        inputProps={{min: 0}}
+        onChange={handler}
+        defaultValue={defaultBpm.perMinute}
+        label="BPM"
+        type="number" />
+      <BeatInput
+        numerator={defaultBpm.unitBeat.numerator}
+        denominator={defaultBpm.unitBeat.denominator}
+        onChange={onChangeUnitBeat} />
+    </div>
   );
 };
